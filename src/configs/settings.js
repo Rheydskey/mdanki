92acc0367768b428b94a6b95253a2a471c8b6e2e
Rheydskey <matgag@tutanota.com>
2024-10-04 19:24:15 +0200

    Use katex instead of mathjax

--- src/configs/settings.js
+++ src/configs/settings.js
@@ -33,7 +33,10 @@ const settings = {
   },
   template: {
     formats: {
-      question: `{{Front}}<link rel="stylesheet" href="${highlightDark}"></link>\n<script>var script;"undefined"==typeof hljs&&((script=document.createElement("script")).src="${prismJs}",script.async=!1,document.head.appendChild(script));\n(script=document.createElement("script")).src="${highlightJs}",script.async=!1,document.head.appendChild(script),document.head.removeChild(script);\n</script>\n<script type="text/javascript" src="https://www.maths.nottingham.ac.uk/plp/pmadw/LaTeXMathML.js"></script>\n    <script type="text/javascript">window.MathJax = {tex: {inlineMath: [["\$", "\$"], ["\\\\(", "\\\\)"]], displayMath: [["\$\$", "\$\$"], ["\\\\[", "\\\\]"]]}};</script><script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>`,
+      question: `{{Front}}<link rel="stylesheet" href="${highlightDark}"></link>\n<script>var script;"undefined"==typeof hljs&&((script=document.createElement("script")).src="${prismJs}",script.async=!1,document.head.appendChild(script));\n(script=document.createElement("script")).src="${highlightJs}",script.async=!1,document.head.appendChild(script),document.head.removeChild(script);\n</script>` +
+        `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" integrity="sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+" crossorigin="anonymous">` + 
+        `<script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.js" integrity="sha384-7zkQWkzuo3B5mTepMUcHkMB5jZaolc2xDwL6VFqjFALcbeS9Ggm/Yr2r3Dy4lfFg" crossorigin="anonymous"></script>` + 
+        `<script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/auto-render.min.js" integrity="sha384-43gviWU0YVjaDtb/GhzOouOXtZMP/7XUzwPTstBeZFe/+rCMvRwr4yROQP43s0Xk" crossorigin="anonymous" onload="renderMathInElement(document.body);"></script>`,
       answer  : '{{FrontSide}}\n\n<hr id="answer">\n\n{{Back}}',
       css     : '.card {\n font-family: Arial,"Helvetica Neue",Helvetica,sans-serif;\n font-size: 16px;\n color: black;\nbackground-color: white;\n}\ncode[class*="language-"],pre[class*="language-"] {\n font-size: 0.9em !important;\n}',
     },
